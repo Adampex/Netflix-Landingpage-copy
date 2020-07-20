833e499c60a188723c2e5bce79e23980ec3d9da6
Adampex <adam.fzy@gmail.com>
2020-07-20 17:29:42 +0200

    Logo replacement and readjusting position and size.

--- js/main.js
+++ js/main.js
@@ -4,15 +4,15 @@ const tabContentItems = document.querySelectorAll('.tab-content-item');
 const hiddenIconItems = document.querySelectorAll('.fa-times');
 const baseIconItems = document.querySelectorAll('.fa-plus');
 
-// Select tab content item
+
 function selectItem(e) {
     
     let itemVisible = false;
-    // grab content item from the DOM
+    
     const tabContentItem = document.querySelector(`#${this.id}-content`);
     const hiddenIconItem = document.querySelector(`#${this.id}-icon`);
     const baseIconItem = document.querySelector(`#${this.id}-basic`);
-    // add show class
+    
     if (tabContentItem.classList.contains('show')) {
         removeShow();
     } else {
@@ -32,5 +32,5 @@ function removeShow() {
     baseIconItems.forEach(item => item.classList.remove('hide'));
 }
 
-// Clicking on the tabs
+
 tabItems.forEach(item => item.addEventListener('click', selectItem));
